a47a6b273a610058d5eb43831a1034f719f5a24e
Ussumane Momade <ussumane.momade@uem.ac.mz>
2023-10-22 17:46:15 +0200

    create product request

---
+++ app/Http/Requests/Products/CreateProductRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Products;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name" => "required|min:2",
+            "description" => "required",
+            "price" => "required|numeric|min:0"
+        ];
+    }
+}
